9819dd9315e50ecf412bd9c1a01b6f1fc5cf262c
lbqds <lbqds@outlook.com>
2022-04-15 10:16:18 +0800

    Fix contract artifacts path

--- sdk/js/src/alephium/token_bridge.ts
+++ sdk/js/src/alephium/token_bridge.ts
@@ -1,31 +1,44 @@
 import { Script } from 'alephium-web3'
+import { promises } from 'fs'
 
-const artifactsPath = "./artifacts"
+const artifactsPath = `${__dirname}/artifacts`
+
+async function readJson(file: string) {
+    const content = await promises.readFile(file)
+    return JSON.parse(content.toString())
+}
 
 export async function createLocalTokenWrapperScript(): Promise<Script> {
-    return Script.fromArtifactFile(`${artifactsPath}/create_local_wrapper.ral.json`)
+    const json = await readJson(`${artifactsPath}/create_local_wrapper.ral.json`)
+    return Script.fromJson(json)
 }
 
 export async function createRemoteTokenWrapperScript(): Promise<Script> {
-    return Script.fromArtifactFile(`${artifactsPath}/create_remote_wrapper.ral.json`)
+    const json = await readJson(`${artifactsPath}/create_remote_wrapper.ral.json`)
+    return Script.fromJson(json)
 }
 
 export async function transferLocalTokenScript(): Promise<Script> {
-    return Script.fromArtifactFile(`${artifactsPath}/transfer_local.ral.json`)
+    const json = await readJson(`${artifactsPath}/transfer_local.ral.json`)
+    return Script.fromJson(json)
 }
 
 export async function transferRemoteTokenScript(): Promise<Script> {
-    return Script.fromArtifactFile(`${artifactsPath}/transfer_remote.ral.json`)
+    const json = await readJson(`${artifactsPath}/transfer_remote.ral.json`)
+    return Script.fromJson(json)
 }
 
 export async function completeTransferScript(): Promise<Script> {
-    return Script.fromArtifactFile(`${artifactsPath}/complete_transfer.ral.json`)
+    const json = await readJson(`${artifactsPath}/complete_transfer.ral.json`)
+    return Script.fromJson(json)
 }
 
 export async function attestTokenScript(): Promise<Script> {
-    return Script.fromArtifactFile(`${artifactsPath}/attest_token.ral.json`)
+    const json = await readJson(`${artifactsPath}/attest_token.ral.json`)
+    return Script.fromJson(json)
 }
 
 export async function completeUndoneSequenceScript(): Promise<Script> {
-    return Script.fromArtifactFile(`${artifactsPath}/complete_undone_sequence.ral.json`)
+    const json = await readJson(`${artifactsPath}/complete_undone_sequence.ral.json`)
+    return Script.fromJson(json)
 }
