c7348c2d96108fbbe8eae63cc9c225792eff2794
lbqds <lbqds@outlook.com>
2022-05-03 11:55:14 +0800

    Fix import from json module

--- sdk/js/src/alephium/token_bridge.ts
+++ sdk/js/src/alephium/token_bridge.ts
@@ -1,36 +1,36 @@
 import { Script } from 'alephium-web3'
-import * as createLocalTokenWrapper from './artifacts/create_local_wrapper.ral.json'
-import * as createRemoteTokenWrapper from './artifacts/create_remote_wrapper.ral.json'
-import * as transferLocalToken from './artifacts/transfer_local.ral.json'
-import * as transferRemoteToken from './artifacts/transfer_remote.ral.json'
-import * as completeTransfer from './artifacts/complete_transfer.ral.json'
-import * as attestToken from './artifacts/attest_token.ral.json'
-import * as completeUndoneSequence from './artifacts/complete_undone_sequence.ral.json'
+import { default as createLocalTokenWrapper } from './artifacts/create_local_wrapper.ral.json'
+import { default as createRemoteTokenWrapper } from './artifacts/create_remote_wrapper.ral.json'
+import { default as transferLocalToken } from './artifacts/transfer_local.ral.json'
+import { default as transferRemoteToken } from './artifacts/transfer_remote.ral.json'
+import { default as completeTransfer } from './artifacts/complete_transfer.ral.json'
+import { default as attestToken } from './artifacts/attest_token.ral.json'
+import { default as completeUndoneSequence } from './artifacts/complete_undone_sequence.ral.json'
 
 export function createLocalTokenWrapperScript(): Script {
-    return Script.fromJsonModule(createLocalTokenWrapper)
+    return Script.fromJson(createLocalTokenWrapper)
 }
 
 export function createRemoteTokenWrapperScript(): Script {
-    return Script.fromJsonModule(createRemoteTokenWrapper)
+    return Script.fromJson(createRemoteTokenWrapper)
 }
 
 export function transferLocalTokenScript(): Script {
-    return Script.fromJsonModule(transferLocalToken)
+    return Script.fromJson(transferLocalToken)
 }
 
 export function transferRemoteTokenScript(): Script {
-    return Script.fromJsonModule(transferRemoteToken)
+    return Script.fromJson(transferRemoteToken)
 }
 
 export function completeTransferScript(): Script {
-    return Script.fromJsonModule(completeTransfer)
+    return Script.fromJson(completeTransfer)
 }
 
 export function attestTokenScript(): Script {
-    return Script.fromJsonModule(attestToken)
+    return Script.fromJson(attestToken)
 }
 
 export function completeUndoneSequenceScript(): Script {
-    return Script.fromJsonModule(completeUndoneSequence)
+    return Script.fromJson(completeUndoneSequence)
 }
