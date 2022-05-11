eb80f937bf8b31182bab3548b71819e702568372
lbqds <lbqds@outlook.com>
2022-05-11 11:04:22 +0800

    Use private key signer in devent for test

--- bridge_ui/src/contexts/AlephiumWalletContext.tsx
+++ bridge_ui/src/contexts/AlephiumWalletContext.tsx
@@ -1,9 +1,9 @@
-import { Signer, CliqueClient } from 'alephium-web3'
+import { CliqueClient, SingleAddressSigner, PrivateKeySigner } from 'alephium-web3'
 import { Context, createContext, ReactChildren, useContext } from 'react'
 import { ALEPHIUM_HOST } from '../utils/consts'
 
 export interface AlephiumWallet {
-    signer: Signer
+    signer: SingleAddressSigner
     address: string
 }
 
@@ -20,11 +20,12 @@ export const AlephiumWalletProvider = ({
 }: {
   children: ReactChildren;
 }) => {
-    const client = new CliqueClient({baseUrl: ALEPHIUM_HOST})
-    const signer = Signer.testSigner(client)
-    const wallet = {
-        signer: signer,
-        address: signer.address
-    }
-    return <WalletContext.Provider value={wallet}>{children}</WalletContext.Provider>
+  const client = new CliqueClient({baseUrl: ALEPHIUM_HOST})
+  // devnet test account signer
+  const signer = new PrivateKeySigner(client, '0c493c4969b89003f964401752f29af896d0aa82d751d23abc1ee59bfe85f3ec')
+  const wallet = {
+    signer: signer,
+    address: signer.address,
+  }
+  return <WalletContext.Provider value={wallet}>{children}</WalletContext.Provider>
 }
