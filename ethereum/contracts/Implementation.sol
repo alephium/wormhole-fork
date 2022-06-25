10f0e918d35114308da3848dbde10ffd80ebf46c
Csongor Kiss <ckiss@jumptrading.com>
2022-06-25 15:10:09 +0100

    ethereum: fix core contract upgradeability
    Missed initializer

--- ethereum/contracts/Implementation.sol
+++ ethereum/contracts/Implementation.sol
@@ -31,6 +31,10 @@ contract Implementation is Governance {
         setNextSequence(emitter, targetChainId, sequence + 1);
     }
 
+    function initialize() initializer public virtual {
+        // this function needs to be exposed for an upgrade to pass
+    }
+
     modifier initializer() {
         address implementation = ERC1967Upgrade._getImplementation();
 
