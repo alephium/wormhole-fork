1872296e73cc16e6b24f21f30d20acd0ef5f34dc
lbqds <lbqds@outlook.com>
2022-08-11 09:17:48 +0800

    Integrate with wallet extension

--- bridge_ui/src/components/AlephiumWalletKey.tsx
+++ bridge_ui/src/components/AlephiumWalletKey.tsx
@@ -1,17 +1,33 @@
+import { useCallback, useState } from "react";
 import { useAlephiumWallet } from "../contexts/AlephiumWalletContext";
+import AlephiumConnectWalletDialog from "./AlephiumConnectWalletDialog";
 import ToggleConnectedButton from "./ToggleConnectedButton";
 
 const AlephiumWalletKey = () => {
-  const { connect, disconnect, signer } =
+  const { disconnect, signer } =
     useAlephiumWallet();
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const openDialog = useCallback(() => {
+    setIsDialogOpen(true);
+  }, [setIsDialogOpen]);
+
+  const closeDialog = useCallback(() => {
+    setIsDialogOpen(false);
+  }, [setIsDialogOpen]);
 
   return (
     <>
       <ToggleConnectedButton
-        connect={connect}
+        connect={openDialog}
         disconnect={disconnect}
         connected={!!signer}
         pk={signer?.account.address || ""}
+      />
+      <AlephiumConnectWalletDialog
+        isOpen={isDialogOpen}
+        onClose={closeDialog}
       />
     </>
   );
