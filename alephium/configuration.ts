a1a3663e11db21af33b61e14f87bc941526bd760
lbqds <lbqds@outlook.com>
2022-09-04 07:39:49 +0800

    Continue executing deploy scripts from the last failed step

--- alephium/configuration.ts
+++ alephium/configuration.ts
@@ -8,6 +8,7 @@ const configuration: Configuration = {
     "devnet": {
       nodeUrl: "http://localhost:22973",
       mnemonic: "vault alarm sad mass witness property virus style good flower rice alpha viable evidence run glare pretty scout evil judge enroll refuse another lava",
+      deploymentFile: ".deployments.json",
       scripts: [
         "scripts/deploy_test_token.ts",
         "scripts/deploy_devnet_deployer.ts",
@@ -18,8 +19,7 @@ const configuration: Configuration = {
         "scripts/register_chain.ts",
         "scripts/get_test_token.ts",
         "scripts/create_test_token_pool.ts",
-        "scripts/create_wrapped_alph_pool.ts",
-        "scripts/auto_mine.ts"
+        "scripts/create_wrapped_alph_pool.ts"
       ]
     },
 
@@ -27,6 +27,7 @@ const configuration: Configuration = {
       // TODO: update config
       nodeUrl: "http://localhost:22973",
       mnemonic: "vault alarm sad mass witness property virus style good flower rice alpha viable evidence run glare pretty scout evil judge enroll refuse another lava",
+      deploymentFile: ".deployments.json",
       scripts: [
         "scripts/deploy_test_token.ts",
         "scripts/deploy_governance.ts",
@@ -44,6 +45,7 @@ const configuration: Configuration = {
       // TODO: update config
       nodeUrl: "http://localhost:22973",
       mnemonic: process.env.MNEMONIC as string,
+      deploymentFile: ".deployments.json",
       scripts: [
         "scripts/deploy_governance.ts",
         "scripts/deploy_wrapped_alph.ts",
--- alephium/scripts/auto_mine.ts
+++ alephium/scripts/auto_mine.ts
@@ -1,14 +1,16 @@
-import { addressFromPublicKey } from "@alephium/web3"
-import { Deployer, NetworkType, PrivateKeySigner } from "../lib/deployment"
+import { addressFromPublicKey, NodeProvider } from "@alephium/web3"
+import configuration from "../configuration"
+import { PrivateKeySigner } from "../lib/deployment"
 
 const fromPrivateKey = '71d640c34b25eedd8acd60be136012f77059836c095fbd02a7cb86a124cbbdae'
 const fromPublicKey = '03034a46d0028a725e27fd24b9142eef856f2ce01d590732744bde0d7553638cd6'
 const fromAddress = addressFromPublicKey(fromPublicKey)
 const toAddress = '199QZVT8bLkYNZ7d2xoHbip29yD18tdeHDPjB7cyx9ofi'
 const transferAmount = "1000000000000000"
+const nodeProvider = new NodeProvider(configuration.networks["devnet"].nodeUrl)
 
-const mine = async (deployer: Deployer, networkType: NetworkType): Promise<void> => {
-  const signer = new PrivateKeySigner(deployer.provider, fromPrivateKey)
+async function mine(provider: NodeProvider): Promise<void> {
+  const signer = new PrivateKeySigner(provider, fromPrivateKey)
   const result = await signer.signTransferTx({
     signerAddress: fromAddress,
     destinations: [{
@@ -19,7 +21,7 @@ const mine = async (deployer: Deployer, networkType: NetworkType): Promise<void>
   })
   console.log("tx submitted, tx id: " + result.txId)
   await new Promise(resolve => setTimeout(resolve, 1000))
-  await mine(deployer, networkType)
+  await mine(provider)
 }
 
-export default mine
+mine(nodeProvider)
