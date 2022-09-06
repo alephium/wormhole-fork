a204c7fe6da603425a77a6399505594a2ee7ce52
lbqds <lbqds@outlook.com>
2022-09-06 10:13:10 +0800

    Use global node provider

--- alephium/scripts/auto_mine.ts
+++ alephium/scripts/auto_mine.ts
@@ -1,5 +1,4 @@
-import { addressFromPublicKey, NodeProvider } from "@alephium/web3"
-import configuration from "../configuration"
+import { addressFromPublicKey } from "@alephium/web3"
 import { PrivateKeySigner } from "../lib/deployment"
 
 const fromPrivateKey = '71d640c34b25eedd8acd60be136012f77059836c095fbd02a7cb86a124cbbdae'
@@ -7,10 +6,9 @@ const fromPublicKey = '03034a46d0028a725e27fd24b9142eef856f2ce01d590732744bde0d7
 const fromAddress = addressFromPublicKey(fromPublicKey)
 const toAddress = '199QZVT8bLkYNZ7d2xoHbip29yD18tdeHDPjB7cyx9ofi'
 const transferAmount = "1000000000000000"
-const nodeProvider = new NodeProvider(configuration.networks["devnet"].nodeUrl)
 
-async function mine(provider: NodeProvider): Promise<void> {
-  const signer = new PrivateKeySigner(provider, fromPrivateKey)
+async function mine(): Promise<void> {
+  const signer = new PrivateKeySigner(fromPrivateKey)
   const result = await signer.signTransferTx({
     signerAddress: fromAddress,
     destinations: [{
@@ -21,7 +19,7 @@ async function mine(provider: NodeProvider): Promise<void> {
   })
   console.log("tx submitted, tx id: " + result.txId)
   await new Promise(resolve => setTimeout(resolve, 1000))
-  await mine(provider)
+  await mine()
 }
 
-mine(nodeProvider)
+mine()
