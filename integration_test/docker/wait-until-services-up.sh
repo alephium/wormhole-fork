3bc83a61a7e4285b1babb65e1fdc77febd970516
h0ngcha0 <lbs.lhc@gmail.com>
2022-11-02 08:20:18 +0100

    Fix eth docker instance name in CI

--- integration_test/docker/wait-until-services-up.sh
+++ integration_test/docker/wait-until-services-up.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SECONDS=0
-until [[ `docker container inspect -f '{{.State.Running}}' docker-eth-devnet-deploy-contracts-1` = 'false' ]]
+until [[ `docker container inspect -f '{{.State.Running}}' docker_eth-devnet-deploy-contracts_1` = 'false' ]]
 do
   if (( SECONDS > 120 ))
   then
