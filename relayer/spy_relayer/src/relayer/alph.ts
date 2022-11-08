6d0fb366721c0f37a76181429981ed7c113bed4f
lbqds <lbqds@outlook.com>
2022-11-08 10:40:24 +0800

    Check if the redeem tx successful

--- relayer/spy_relayer/src/relayer/alph.ts
+++ relayer/spy_relayer/src/relayer/alph.ts
@@ -9,6 +9,7 @@ import { PrivateKeyWallet } from "@alephium/web3-wallet"
 import { AlephiumChainConfigInfo } from "../configureEnv";
 import { getScopedLogger, ScopedLogger } from "../helpers/logHelper";
 import { PromHelper } from "../helpers/promHelpers";
+import { node, NodeProvider } from '@alephium/web3'
 
 export async function relayAlph(
   emitterChainId: ChainId,
@@ -46,9 +47,42 @@ export async function relayAlph(
   logger.info(`Will redeem using pubkey: ${(await signer.getSelectedAccount()).address}`)
 
   logger.debug('Redeeming...')
-  const result = await redeemOnAlph(signer, tokenBridgeForChainId, signedVaaArray)
-  logger.info(`Redeem transaction tx id: ${result.txId}`)
+  const redeemResult = await redeemOnAlph(signer, tokenBridgeForChainId, signedVaaArray)
+  const confirmed = await waitAlphTxConfirmed(signer.nodeProvider, redeemResult.txId, 1, 120)
+  const executionOk = await getScriptExecutionResult(signer.nodeProvider, confirmed)
+  logger.info(`Redeem transaction tx id: ${redeemResult.txId}, script execution result: ${executionOk}`)
 
-  metrics.incSuccesses(chainConfigInfo.chainId)
-  return { redeemed: true, result: result.txId }
+  if (executionOk) {
+    metrics.incSuccesses(chainConfigInfo.chainId)
+    return { redeemed: true, result: `${redeemResult.txId} executed successfully` }
+  } else {
+    metrics.incFailures(chainConfigInfo.chainId)
+    return { redeemed: executionOk, result: `${redeemResult.txId} execution failed` }
+  }
+}
+
+async function getScriptExecutionResult(nodeProvider: NodeProvider, confirmed: node.Confirmed) {
+  const block = await nodeProvider.blockflow.getBlockflowBlocksBlockHash(confirmed.blockHash)
+  return block.transactions[confirmed.txIndex].scriptExecutionOk
+}
+
+function isConfirmed(txStatus: node.TxStatus): txStatus is node.Confirmed {
+  return txStatus.type === 'Confirmed'
+}
+
+export async function waitAlphTxConfirmed(
+  provider: NodeProvider,
+  txId: string,
+  confirmations: number,
+  timeout: number // seconds
+): Promise<node.Confirmed> {
+  if (timeout <= 0) {
+    throw new Error(`Wait tx confirmed timeout, txId: ${txId}`)
+  }
+  const status = await provider.transactions.getTransactionsStatus({ txId: txId })
+  if (isConfirmed(status) && status.chainConfirmations >= confirmations) {
+    return status
+  }
+  await new Promise((r) => setTimeout(r, 10000))
+  return waitAlphTxConfirmed(provider, txId, confirmations, timeout - 10)
 }
