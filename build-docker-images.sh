6d6121f084c8585f3b9271159602f286a83f6aaf
lbqds <lbqds@outlook.com>
2022-11-14 20:32:17 +0800

    Rename image orgnization name

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -19,7 +19,7 @@ docker build --target const-export -f Dockerfile.const -o type=local,dest=. --bu
 
 # Build guardian image (used for both guardian & spy)
 pushd node
-docker build . -t wormhole/guardiand:$VERSION
+docker build . -t alephium/guardiand:$VERSION
 popd
 
 ## Build eth-node image
@@ -27,21 +27,21 @@ pushd ethereum
 cp .env.test .env
 git apply 1conf.patch
 git apply truffle-config.patch
-docker build . -t wormhole/eth-node:$VERSION
+docker build . -t alephium/eth-node:$VERSION
 git apply -R 1conf.patch
 git apply -R truffle-config.patch
 popd
 
 pushd alephium
-docker build -f Dockerfile.automine . -t wormhole/automine:$VERSION
+docker build -f Dockerfile.automine . -t alephium/automine:$VERSION
 popd
 
 ## Build Bridge UI
 pushd bridge_ui
-docker build . -t wormhole/bridge-ui:$VERSION
+docker build . -t alephium/bridge-ui:$VERSION
 popd
 
 ## Build Wormhole Explorer
 pushd explorer
-docker build . -t wormhole/wormhole-explorer:$VERSION
+docker build . -t alephium/wormhole-explorer:$VERSION
 popd
