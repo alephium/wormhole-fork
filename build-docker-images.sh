bfbbcf7e09407666a24247d25e662b675b1de1e0
lbqds <lbqds@outlook.com>
2022-11-25 15:43:08 +0800

    Fix docker files

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -3,7 +3,12 @@
 NETWORKS=('mainnet' 'testnet' 'devnet')
 
 network=$1
-NUM_GUARDIANS=1
+
+if [ -z "${network// }" ]
+then
+    echo "Please specify the network type"
+    exit 1
+fi
 
 set -euo pipefail xtrace
 
@@ -18,14 +23,13 @@ else
     exit 1
 fi
 
+docker build -f Dockerfile.init . -t alephium/devnet-init:$VERSION
+
 # Build proto-gen, generate node/pkg/proto dir
 docker build --target go-export -f Dockerfile.proto -o type=local,dest=node .
 
 # Build proto-gen-web
 docker build --target node-export -f Dockerfile.proto -o type=local,dest=. .
-
-# Build const-gen
-docker build --target const-export -f Dockerfile.const -o type=local,dest=. --build-arg num_guardians=$NUM_GUARDIANS .
 
 # Build guardian image (used for both guardian & spy)
 pushd node
@@ -33,25 +37,9 @@ docker build . -t alephium/guardiand:$VERSION --build-arg network=$network
 popd
 
 ## Build eth-node image
-pushd ethereum
-if [[ "$network" == 'devnet' ]] ; then
-    cp .env.devnet .env
-    git apply 1conf.patch
-    git apply truffle-config.patch
-fi
-docker build . -t alephium/eth-node:$VERSION
-if [[ "$network" == 'devnet' ]] ; then
-    git apply -R 1conf.patch
-    git apply -R truffle-config.patch
-fi
-popd
+docker build -f ./ethereum/Dockerfile . -t alephium/eth-node:$VERSION
 
-## Build auto miner for alephium
-if [[ "$network" == 'devnet' ]] ; then
-    pushd alephium
-    docker build -f Dockerfile.automine . -t alephium/automine:$VERSION
-    popd
-fi
+docker build -f ./alephium/Dockerfile . -t alephium/alephium-contracts:$VERSION
 
 ## Build Bridge UI
 pushd bridge_ui
--- integration_test/docker/wait-until-services-up.sh
+++ integration_test/docker/wait-until-services-up.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 SECONDS=0
-until [[ `docker container inspect -f '{{.State.Running}}' docker_eth-devnet-deploy-contracts_1` = 'false' ]]
+until [[ `docker container inspect -f '{{.State.Running}}' docker_docker-devnet-init_1` = 'false' ]]
 do
   if (( SECONDS > 120 ))
   then
-     echo "ETH contracts are not deployed after 2 min..."
+     echo "Devnet does not start after 2 min..."
      exit 1
   fi
   echo "Waiting..."
