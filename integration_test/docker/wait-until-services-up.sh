a1a57190aa89ff96905959e601cab5a798296a61
lbqds <lbqds@outlook.com>
2022-11-28 22:59:44 +0800

    Fix container name

--- integration_test/docker/wait-until-services-up.sh
+++ integration_test/docker/wait-until-services-up.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SECONDS=0
-until [[ `docker container inspect -f '{{.State.Running}}' docker_docker-devnet-init_1` = 'false' ]]
+until [[ `docker container inspect -f '{{.State.Running}}' docker_devnet-init_1` = 'false' ]]
 do
   if (( SECONDS > 120 ))
   then
