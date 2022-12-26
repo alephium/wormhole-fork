75ba2e9f0a1ed26d6f60f0060845cab818d4f2ba
h0ngcha0 <lbs.lhc@gmail.com>
2022-12-26 05:04:11 +0100

    Bump docker version

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -12,7 +12,7 @@ fi
 
 set -euo pipefail xtrace
 
-VERSION=0.2.81
+VERSION=0.2.82
 export DOCKER_BUILDKIT=1
 
 if [[ ${NETWORKS[*]}] =~ $network ]]
--- scripts/register-chain.sh
+++ scripts/register-chain.sh
@@ -7,9 +7,9 @@ NEXT_SEQUENCE=`/guardiand admin get-next-governance-vaa-sequence --socket /tmp/a
 TEMPLATE_FILE=/tmp/register-chain-vaa.template
 
 # Register ETH to Alephium on testnet
-#/guardiand template token-bridge-register-chain --idx=0 --new-address 0000000000000000000000004722495183669f1b85d8A2dFA2C6F5dd8FA627b4 --module TokenBridge --chain-id 2 > $TEMPLATE_FILE
+/guardiand template token-bridge-register-chain --idx=0 --new-address 000000000000000000000000b4d3cbd7BF0503eBFd34637C43B8b6E836611204 --module TokenBridge --chain-id 2 > $TEMPLATE_FILE
 # Register ALELPHIUM to ETH on testnet
-/guardiand template token-bridge-register-chain --idx=0 --new-address 0140a16e45690ba9c12d87766ee0c529e362d5b5b6156ce507cb956dc601438f --module TokenBridge --chain-id 255 > $TEMPLATE_FILE
+# /guardiand template token-bridge-register-chain --idx=0 --new-address dcfd296d3934726fae0d441329cad9503eed56c50829adee070cb721d8e4f726 --module TokenBridge --chain-id 255 > $TEMPLATE_FILE
 
 sed -i "s/sequence: [[:digit:]]\+/sequence: $NEXT_SEQUENCE/" $TEMPLATE_FILE
 
@@ -17,13 +17,13 @@ echo "Template:"
 cat $TEMPLATE_FILE
 echo
 
-/guardiand admin governance-vaa-verify $TEMPLATE_FILE
-
-# Yes or no
-read -p "Inject the VAA? [y/n] " -n 1 -r
-echo
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  /guardiand admin governance-vaa-inject $TEMPLATE_FILE --socket /tmp/admin.sock
-fi
+#/guardiand admin governance-vaa-verify $TEMPLATE_FILE
+#
+## Yes or no
+#read -p "Inject the VAA? [y/n] " -n 1 -r
+#echo
+#echo
+#if [[ $REPLY =~ ^[Yy]$ ]]
+#then
+#  /guardiand admin governance-vaa-inject $TEMPLATE_FILE --socket /tmp/admin.sock
+#fi
