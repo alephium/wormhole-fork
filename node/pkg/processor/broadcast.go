wormhole-foundation/wormhole

Send re-observation request when re-broadcasting local observations
Currently if an observation hasn't reached quorum within 5 minutes, the
processor will re-broadcast the signed local observation to the other
guardians in the network. However if not enough guardians actually
observed the original tx, then no amount of re-broadcasting will help
the network reach quorum.

Fix this issue by sending a re-observation request whenever we
re-broadcast a signed local observation.  This ensures that any
guardians that missed the tx the first time it happened have a chance to
re-observe it and help the network reach quorum.

node/pkg/processor/broadcast.go
@@ -59,6 +59,7 @@ func (p *Processor) broadcastSignature(
 
 	p.state.signatures[hash].ourObservation = o
 	p.state.signatures[hash].ourMsg = msg
+	p.state.signatures[hash].txHash = txhash
 	p.state.signatures[hash].source = o.GetEmitterChain().String()
 	p.state.signatures[hash].gs = p.gs // guaranteed to match ourObservation - there's no concurrent access to p.gs
 

alephium/wormhole-fork

node/pkg/processor/broadcast.go
@@ -2,9 +2,10 @@ package processor
 
 import (
 	"encoding/hex"
+	"time"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-	"time"
 
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -55,6 +56,7 @@ func (p *Processor) broadcastSignature(v *vaa.VAA, signature []byte, txhash []by
 
 	p.state.vaaSignatures[hash].ourVAA = v
 	p.state.vaaSignatures[hash].ourMsg = msg
+	p.state.vaaSignatures[hash].txHash = txhash
 	p.state.vaaSignatures[hash].source = v.EmitterChain.String()
 	p.state.vaaSignatures[hash].gs = p.gs // guaranteed to match ourVAA - there's no concurrent access to p.gs
 