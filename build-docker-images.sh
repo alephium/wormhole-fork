640b272b2a1a71b271678629d1f691f6ae3e01ce
h0ngcha0 <lbs.lhc@gmail.com>
2023-01-05 10:39:01 +0100

    Test docker auth

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -14,7 +14,7 @@ fi
 
 set -euo pipefail xtrace
 
-VERSION=0.2.85
+VERSION=0.2.86
 export DOCKER_BUILDKIT=1
 
 if [[ ${NETWORKS[*]}] =~ $network ]]
@@ -25,31 +25,31 @@ else
     exit 1
 fi
 
-docker build -f Dockerfile.init . -t eu.gcr.io/alephium-org/devnet-init:$VERSION
+#docker build -f Dockerfile.init . -t eu.gcr.io/alephium-org/devnet-init:$VERSION
 
-# Build proto-gen, generate node/pkg/proto dir
-docker build --target go-export -f Dockerfile.proto -o type=local,dest=node .
-
-# Build proto-gen-web
-docker build --target node-export -f Dockerfile.proto -o type=local,dest=. .
+## Build proto-gen, generate node/pkg/proto dir
+#docker build --target go-export -f Dockerfile.proto -o type=local,dest=node .
+#
+## Build proto-gen-web
+#docker build --target node-export -f Dockerfile.proto -o type=local,dest=. .
 
 # Build guardian image (used for both guardian & spy)
 docker build -f ./node/Dockerfile . -t eu.gcr.io/alephium-org/guardiand:$VERSION
 
 ## Build eth-node image
-docker build -f ./ethereum/Dockerfile . -t eu.gcr.io/alephium-org/eth-node:$VERSION
-
-docker build -f ./alephium/Dockerfile . -t eu.gcr.io/alephium-org/alephium-contracts:$VERSION
-
-## Build Bridge UI
-docker build -f ./bridge_ui/Dockerfile . -t eu.gcr.io/alephium-org/bridge-ui-$network:$VERSION --build-arg network=$network
-
-## Build Wormhole Explorer
-docker build -f ./explorer/Dockerfile . -t eu.gcr.io/alephium-org/wormhole-explorer:$VERSION
+#docker build -f ./ethereum/Dockerfile . -t eu.gcr.io/alephium-org/eth-node:$VERSION
+#
+#docker build -f ./alephium/Dockerfile . -t eu.gcr.io/alephium-org/alephium-contracts:$VERSION
+#
+### Build Bridge UI
+#docker build -f ./bridge_ui/Dockerfile . -t eu.gcr.io/alephium-org/bridge-ui-$network:$VERSION --build-arg network=$network
+#
+### Build Wormhole Explorer
+#docker build -f ./explorer/Dockerfile . -t eu.gcr.io/alephium-org/wormhole-explorer:$VERSION
 
 if [ "${pushImage}" = true ]
 then
     docker push eu.gcr.io/alephium-org/guardiand:$VERSION
-    docker push eu.gcr.io/alephium-org/bridge-ui:$VERSION
-    docker push eu.gcr.io/alephium-org/wormhole-explorer:$VERSION
+#    docker push eu.gcr.io/alephium-org/bridge-ui-$network:$VERSION
+#    docker push eu.gcr.io/alephium-org/wormhole-explorer:$VERSION
 fi
