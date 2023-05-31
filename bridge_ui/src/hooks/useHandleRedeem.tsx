7d3ce1d77139a7f5f4a52844ea7fcd8c733d94ba
lbqds <lbqds@outlook.com>
2023-05-31 14:35:43 +0800

    Remove the transaction db

--- bridge_ui/src/hooks/useHandleRedeem.tsx
+++ bridge_ui/src/hooks/useHandleRedeem.tsx
@@ -60,7 +60,6 @@ import { signSendAndConfirm } from "../utils/solana";
 import { postWithFees } from "../utils/terra";
 import { getEmitterChainId, waitALPHTxConfirmed } from "../utils/alephium";
 import useTransferSignedVAA from "./useTransferSignedVAA";
-import { TransactionDB } from "../utils/db";
 import { AlephiumWallet, useAlephiumWallet } from "./useAlephiumWallet";
 import { redeemOnEthNativeWithoutWait, redeemOnEthWithoutWait } from "../utils/ethereum";
 
@@ -138,9 +137,6 @@ async function evm(
     dispatch(
       setRedeemTx({ id: receipt.transactionHash, block: receipt.blockNumber })
     );
-    if (sourceTxId && sourceChainId === CHAIN_ID_ALEPHIUM) {
-      await TransactionDB.getInstance().txs.update(sourceTxId, {status: "Completed"})
-    }
     enqueueSnackbar(null, {
       content: <Alert severity="success">Transaction confirmed</Alert>,
     });
