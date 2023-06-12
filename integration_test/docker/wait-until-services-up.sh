a05d507315d83bf842315d0e64efe88f590b71b7
lbqds <lbqds@outlook.com>
2023-06-12 13:26:55 +0800

    Increase timeout

--- integration_test/docker/wait-until-services-up.sh
+++ integration_test/docker/wait-until-services-up.sh
@@ -3,9 +3,9 @@
 SECONDS=0
 until [[ `docker container inspect -f '{{.State.Running}}' docker_devnet-init_1` = 'false' ]]
 do
-  if (( SECONDS > 300 ))
+  if (( SECONDS > 480 ))
   then
-     echo "Devnet does not start after 5 min..."
+     echo "Devnet does not start after 8 min..."
      exit 1
   fi
   echo "Waiting..."
