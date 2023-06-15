d69d1cee4442b1d91ca8f9b5b43bf1ab1cbd9c54
lbqds <lbqds@outlook.com>
2023-06-15 17:38:40 +0800

    Remove legacy relayer code

--- relayer/spy_relayer/src/helpers/loadConfig.ts
+++
@@ -1,7 +0,0 @@
-import { config } from "dotenv";
-const configFile: string = process.env.SPY_RELAY_CONFIG
-  ? process.env.SPY_RELAY_CONFIG
-  : ".env.sample";
-console.log(`Loading config file [${configFile}]`);
-config({ path: configFile });
-export {};
