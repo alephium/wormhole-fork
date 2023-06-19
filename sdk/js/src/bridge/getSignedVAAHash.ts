7b60b2f988a6737c8974726cbdaae336dd78d851
lbqds <lbqds@outlook.com>
2023-06-19 11:20:47 +0800

    Update @alephium/web3 to 0.13.0

--- sdk/js/src/bridge/getSignedVAAHash.ts
+++ sdk/js/src/bridge/getSignedVAAHash.ts
@@ -2,7 +2,7 @@ import { ethers } from "ethers";
 import { uint8ArrayToHex } from "..";
 import { extractBodyFromVAA } from "../utils";
 
-export async function getSignedVAAHash(signedVAA: Uint8Array) {
+export function getSignedVAAHash(signedVAA: Uint8Array) {
   const body = extractBodyFromVAA(signedVAA)
   const bodyHex = uint8ArrayToHex(body)
   return ethers.utils.solidityKeccak256(["bytes"], [ethers.utils.solidityKeccak256(["bytes"], ["0x" + bodyHex])])
