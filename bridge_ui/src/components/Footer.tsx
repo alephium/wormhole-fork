065de20e9caa92297ba386067f5db782b3c8c2e5
Cheng Wang <wachmc@gmail.com>
2023-09-29 13:56:52 +0200

    Format

--- bridge_ui/src/components/Footer.tsx
+++ bridge_ui/src/components/Footer.tsx
@@ -56,24 +56,28 @@ export default function Footer() {
         <div className={classes.flex}>
           <div className={classes.spacer} />
           <Typography variant="body2">
-            This Interface is open-source software providing access to Alephium's Bridge, a cross-chain messaging protocol. It is a fork based on the open-source code of the Wormhole Bridge and is not affiliated with or endorsed by the Wormhole Foundation. THIS INTERFACE AND THE BRIDGE PROTOCOL ARE
-            PROVIDED "AS IS", AT YOUR OWN RISK, AND WITHOUT WARRANTIES OF ANY
-            KIND. By using or accessing this Interface or Bridge, you agree that
-            no developer or entity involved in creating, deploying, maintaining,
-            operating this Interface or Bridge, or causing or supporting any of
-            the foregoing, will be liable in any manner for any claims or
-            damages whatsoever associated with your use, inability to use, or
-            your interaction with other users of, this Interface or Bridge, or
-            this Interface or Bridge themselves, including any direct, indirect,
-            incidental, special, exemplary, punitive or consequential damages,
-            or loss of profits, cryptocurrencies, tokens, or anything else of
-            value. By using or accessing this Interface, you represent that you
-            are not subject to sanctions or otherwise designated on any list of
-            prohibited or restricted parties or excluded or denied persons,
-            including but not limited to the lists maintained by the United
-            States' Department of Treasury's Office of Foreign Assets Control,
-            the United Nations Security Council, the European Union or its
-            Member States, or any other government authority.
+            This Interface is open-source software providing access to
+            Alephium's Bridge, a cross-chain messaging protocol. It is a fork
+            based on the open-source code of the Wormhole Bridge and is not
+            affiliated with or endorsed by the Wormhole Foundation. THIS
+            INTERFACE AND THE BRIDGE PROTOCOL ARE PROVIDED "AS IS", AT YOUR OWN
+            RISK, AND WITHOUT WARRANTIES OF ANY KIND. By using or accessing this
+            Interface or Bridge, you agree that no developer or entity involved
+            in creating, deploying, maintaining, operating this Interface or
+            Bridge, or causing or supporting any of the foregoing, will be
+            liable in any manner for any claims or damages whatsoever associated
+            with your use, inability to use, or your interaction with other
+            users of, this Interface or Bridge, or this Interface or Bridge
+            themselves, including any direct, indirect, incidental, special,
+            exemplary, punitive or consequential damages, or loss of profits,
+            cryptocurrencies, tokens, or anything else of value. By using or
+            accessing this Interface, you represent that you are not subject to
+            sanctions or otherwise designated on any list of prohibited or
+            restricted parties or excluded or denied persons, including but not
+            limited to the lists maintained by the United States' Department of
+            Treasury's Office of Foreign Assets Control, the United Nations
+            Security Council, the European Union or its Member States, or any
+            other government authority.
           </Typography>
         </div>
       </div>
