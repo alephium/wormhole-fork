9a4c7d0f3c0465b3d7c2814b507ed662189629bb
mvaivre <m.vaivre@gmail.com>
2025-09-25 17:17:38 +0200

    Clean up MainActionButton state changes and anim

--- bridge_ui/src/components/BridgeWidget/MainActionButton.tsx
+++ bridge_ui/src/components/BridgeWidget/MainActionButton.tsx
@@ -9,12 +9,13 @@ import {
 } from '../../store/selectors'
 import { selectTransferTargetChain } from '../../store/selectors'
 import useIsWalletReady from '../../hooks/useIsWalletReady'
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { CHAINS_BY_ID, getIsTransferDisabled } from '../../utils/consts'
 import { CHAIN_ID_ALEPHIUM, ChainId, isEVMChain } from '@alephium/wormhole-sdk'
 import EvmConnectWalletDialog from '../EvmConnectWalletDialog'
 import { AlephiumConnectButton } from '@alephium/web3-react'
-import { TokenIconSymbol } from './TokenPicker2'
+import { useWidgetStyles } from './styles'
+import clsx from 'clsx'
 
 interface MainActionButtonProps {
   onNext?: () => void
@@ -23,6 +24,7 @@ interface MainActionButtonProps {
 
 const MainActionButton = ({ onNext, onSelectToken }: MainActionButtonProps) => {
   const { t } = useTranslation()
+  const classes = useWidgetStyles()
   const sourceChain = useSelector(selectTransferSourceChain)
   const targetChain = useSelector(selectTransferTargetChain)
   const { isReady: isSourceReady } = useIsWalletReady(sourceChain)
@@ -31,6 +33,7 @@ const MainActionButton = ({ onNext, onSelectToken }: MainActionButtonProps) => {
   const isTargetComplete = useSelector(selectTransferIsTargetComplete)
   const selectedToken = useSelector(selectTransferSourceParsedTokenAccount)
   const hasSelectedToken = !!selectedToken
+  const [isAnimating, setIsAnimating] = useState(false)
   const isSourceTransferDisabled = useMemo(() => {
     return getIsTransferDisabled(sourceChain, true)
   }, [sourceChain])
@@ -38,37 +41,53 @@ const MainActionButton = ({ onNext, onSelectToken }: MainActionButtonProps) => {
     return getIsTransferDisabled(targetChain, false)
   }, [targetChain])
 
-  if (!isSourceReady) {
-    return <ConnectButton chainId={sourceChain} />
-  }
-
-  if (!isTargetReady) {
-    return <ConnectButton chainId={targetChain} />
-  }
-
-  const showSelectTokenButton = !!onSelectToken && !hasSelectedToken
-  const showNextButton = !!onNext && hasSelectedToken
-
-  if (!showSelectTokenButton && !showNextButton) {
-    return null
-  }
+  const canSelectToken = typeof onSelectToken === 'function'
+  const canProceed = typeof onNext === 'function'
 
   const isNextDisabled =
     !isSourceComplete || !isTargetComplete || isSourceTransferDisabled || isTargetTransferDisabled
 
+  let variant: 'none' | 'connect-source' | 'connect-target' | 'select-token' | 'next-disabled' | 'next-enabled' = 'none'
+  let buttonNode: JSX.Element | null = null
+
+  if (!isSourceReady) {
+    variant = 'connect-source'
+    buttonNode = <ConnectButton chainId={sourceChain} />
+  } else if (!isTargetReady) {
+    variant = 'connect-target'
+    buttonNode = <ConnectButton chainId={targetChain} />
+  } else if (canSelectToken && !hasSelectedToken) {
+    variant = 'select-token'
+    buttonNode = <BridgeWidgetButton onClick={onSelectToken}>{t('Select token')}</BridgeWidgetButton>
+  } else if (canProceed && hasSelectedToken) {
+    variant = isNextDisabled ? 'next-disabled' : 'next-enabled'
+    buttonNode = (
+      <BridgeWidgetButton disabled={isNextDisabled} onClick={onNext}>
+        {t('Next')}
+      </BridgeWidgetButton>
+    )
+  }
+
+  useEffect(() => {
+    if (variant === 'none') return
+
+    setIsAnimating(true)
+    const timeout = window.setTimeout(() => {
+      setIsAnimating(false)
+    }, 260)
+
+    return () => window.clearTimeout(timeout)
+  }, [variant])
+
+  if (!buttonNode) {
+    return null
+  }
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', width: '100%' }}>
-      {showSelectTokenButton && (
-        <BridgeWidgetButton onClick={onSelectToken}>
-          {selectedToken ? <TokenIconSymbol account={selectedToken} /> : t('Select token')}
-        </BridgeWidgetButton>
-      )}
-
-      {showNextButton && (
-        <BridgeWidgetButton disabled={isNextDisabled} onClick={onNext}>
-          {t('Next')}
-        </BridgeWidgetButton>
-      )}
+      <div className={clsx(classes.pulseWrapper, isAnimating && classes.pulseWrapperAnimated)}>
+        {buttonNode}
+      </div>
     </div>
   )
 }
