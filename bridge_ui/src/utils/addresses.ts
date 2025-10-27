171ec739141d4e491eb9b2ed887ee672bb61106b
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-10-27 10:47:21 +0800

    Fix display of EVM addresses

--- bridge_ui/src/utils/addresses.ts
+++ bridge_ui/src/utils/addresses.ts
@@ -1,5 +1,5 @@
 export function shortenAddress(address: string) {
   return address.length > 10
-    ? `${address.slice(0, 4)}...${address.slice(-4)}`
+    ? `${address.slice(0, address.startsWith('0x') ? 6 : 4)}...${address.slice(-4)}`
     : address;
 }
