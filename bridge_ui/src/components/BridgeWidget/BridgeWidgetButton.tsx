609e47f0cb79cc3e7c4667d03a1d6cb83fc801fc
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-11-07 11:00:30 +0100

    Convert button styles to use emotion

--- bridge_ui/src/components/BridgeWidget/BridgeWidgetButton.tsx
+++ bridge_ui/src/components/BridgeWidget/BridgeWidgetButton.tsx
@@ -1,6 +1,4 @@
-import { Button, ButtonProps, CircularProgress } from '@mui/material'
-import { makeStyles } from '@mui/styles';
-import clsx from 'clsx'
+import { Button, ButtonProps, CircularProgress, styled } from '@mui/material'
 import { COLORS } from '../../muiTheme'
 
 export type BridgeWidgetButtonProps = ButtonProps & {
@@ -16,62 +14,50 @@ const BridgeWidgetButton = ({
   isLoading,
   short,
   ...props
-}: BridgeWidgetButtonProps) => {
-  const classes = useStyles()
-
-  return (
-    <Button
-      className={clsx(classes.button, tone === 'primaryNext' && classes.primaryNext, short && classes.short, className)}
-      variant={variant}
-      color="primary"
-      fullWidth
-      {...props}
-    >
-      {isLoading ? <CircularProgress size={20} color="inherit" /> : props.children}
-    </Button>
-  )
-}
+}: BridgeWidgetButtonProps) => (
+  <ButtonStyled className={className} variant={variant} color="primary" fullWidth tone={tone} short={short} {...props}>
+    {isLoading ? <CircularProgress size={20} color="inherit" /> : props.children}
+  </ButtonStyled>
+)
 
 export default BridgeWidgetButton
 
-const useStyles = makeStyles(() => ({
-  button: {
-    textTransform: 'none',
-    borderRadius: '16px',
-    height: '52px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontFamily: 'Inter, sans-serif',
-    fontWeight: 550,
-    fontSize: '16px',
-    letterSpacing: 'normal',
-    transition: 'all 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94)',
-    '&.MuiButton-containedPrimary': {
-      backgroundColor: COLORS.nearWhite,
-      color: COLORS.nearBlack,
-      '&:hover': {
-        transform: 'scale(1.005)',
-        backgroundColor: COLORS.white,
-        filter: 'brightness(1.1)'
-      }
-    },
-    '&.MuiButton-outlinedPrimary': {
-      backgroundColor: COLORS.whiteWithStrongTransparency,
-      borderColor: COLORS.whiteWithStrongTransparency,
-      color: COLORS.nearWhite,
-      '&:hover': {
-        transform: 'scale(1.005)',
-        color: COLORS.white,
-        backgroundColor: COLORS.whiteWithTransparency
-      }
-    },
-    '&.Mui-disabled': {
-      backgroundColor: 'rgba(255, 255, 255, 0.65)',
-      color: 'rgba(0, 0, 0, 0.35)'
+const ButtonStyled = styled(Button)<BridgeWidgetButtonProps>(({ tone, short }) => ({
+  textTransform: 'none',
+  borderRadius: '16px',
+  height: '52px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontFamily: 'Inter, sans-serif',
+  fontWeight: 550,
+  fontSize: '16px',
+  letterSpacing: 'normal',
+  transition: 'all 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94)',
+  '&.MuiButton-containedPrimary': {
+    backgroundColor: COLORS.nearWhite,
+    color: COLORS.nearBlack,
+    '&:hover': {
+      transform: 'scale(1.005)',
+      backgroundColor: COLORS.white,
+      filter: 'brightness(1.1)'
     }
   },
-  primaryNext: {
+  '&.MuiButton-outlinedPrimary': {
+    backgroundColor: COLORS.whiteWithStrongTransparency,
+    borderColor: COLORS.whiteWithStrongTransparency,
+    color: COLORS.nearWhite,
+    '&:hover': {
+      transform: 'scale(1.005)',
+      color: COLORS.white,
+      backgroundColor: COLORS.whiteWithTransparency
+    }
+  },
+  '&.Mui-disabled': {
+    backgroundColor: 'rgba(255, 255, 255, 0.65)',
+    color: 'rgba(0, 0, 0, 0.35)'
+  },
+  ...(tone === 'primaryNext' && {
     '&.MuiButton-containedPrimary': {
       backgroundColor: COLORS.blue,
       color: COLORS.white,
@@ -81,9 +67,9 @@ const useStyles = makeStyles(() => ({
         color: COLORS.white
       }
     }
-  },
-  short: {
+  }),
+  ...(short && {
     height: '36px',
     borderRadius: '12px'
-  }
+  })
 }))
